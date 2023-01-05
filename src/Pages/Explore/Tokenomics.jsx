36878c37703089e76ab5d77cea621ed550548785
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-05 14:58:23 +0500

    qweW

--- src/Pages/Explore/Tokenomics.jsx
+++ src/Pages/Explore/Tokenomics.jsx
@@ -57,6 +57,58 @@ const Tokenomics = () => {
             </div>
           </div>
         </div>
+        <div className="flex mt-[80px] xl:mt-[150px] xl:flex-row flex-col justify-between items-center gap-[60px] xl:gap-5 w-full">
+          <div className="flex overflow-x-auto justify-start items-start flex-col gap-5  max-w-[550px] w-full py-5 px-6 bg-[#05050F] border-2 border-solid border-[#b3b3b3] rounded-[30px] relative">
+            <div className="grid  grid-cols-2 gap-5 w-full py-5 items-center relative">
+              <div className="w-full h-[2px] bg-tableBorder absolute bottom-full left-0 z-10"></div>
+              <div className="w-full h-[2px] bg-tableBorder absolute top-full left-0 z-10"></div>
+              <p className="text-base sm:text-xl text-center sm:text-left font-bold text-white">
+                Inital Market Cap
+              </p>
+              <p className="text-base sm:text-xl text-center sm:text-left font-bold text-white">
+                Initial Circulating Supply
+              </p>
+            </div>
+            <div className="grid  grid-cols-2 gap-5 w-full py-5">
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                FDV
+              </p>
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                $12 000 000
+              </p>
+            </div>
+            <div className="grid  grid-cols-2 gap-5 w-full py-5">
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                IMC
+              </p>
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                $714 000
+              </p>
+            </div>
+            <div className="grid  grid-cols-2 gap-5 w-full py-5">
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                Liquidity
+              </p>
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                $384 000
+              </p>
+            </div>
+            <div className="grid  grid-cols-2 gap-5 w-full py-5">
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                IMC Without Liq
+              </p>
+              <p className="text-base sm:text-left text-center sm:text-lg font-normal text-white">
+                $330 000
+              </p>
+            </div>
+            <div className="w-full h-[2px] bg-tableBorder absolute bottom-[3%] left-0 z-10"></div>
+          </div>
+          <img
+            src="/explore/pie-chart.png"
+            className="object-contain w-full max-w-[570px]"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
