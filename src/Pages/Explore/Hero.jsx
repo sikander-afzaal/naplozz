28c17826ff642589e234bd5c264be44da9dc10bc
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-07 01:30:44 +0500

    asd

--- src/Pages/Explore/Hero.jsx
+++ src/Pages/Explore/Hero.jsx
@@ -4,16 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Hero = () => {
   return (
-    <div className="wrapper pt-[0px] mid:pt-[50px]  relative ">
+    <div className="wrapper pt-[0px] xl:pt-[50px]  relative ">
       <img
         src="/explore/hero-bg.png"
         className="absolute w-full h-full top-0 left-0 object-cover -z-10"
         alt=""
       />
       <div className="absolute explore -left-[20%] top-0 bg-greenRadial -z-10 w-full max-w-[854px] h-[804px] blur-[111px] mix-blend-screen"></div>
-      <div className="contain  flex-col mid:flex-row   justify-between items-center mid:items-start">
-        <div className="flex justify-start items-center pt-[150px] text-center mid:text-left mid:items-start mid:max-w-none max-w-[600px] flex-col gap-5">
-          <h2 className="font-bold leading-[1.2] mid:leading-[92px] text-[40px] sm:text-[55px] mid:text-[68px] text-white">
+      <div className="contain  flex-col xl:flex-row   justify-between items-center xl:items-start">
+        <div className="flex justify-start items-center pt-[150px] text-center xl:text-left xl:items-start xl:max-w-none max-w-[600px] flex-col gap-5">
+          <h2 className="font-bold leading-[1.2]  mid:leading-[92px] text-[40px] sm:text-[55px] xl:text-[63px] text-white">
             <span className="gr-text">Naplozz App</span> the paperless record
             keeper
           </h2>
@@ -37,7 +37,7 @@ const Hero = () => {
         <img
           src="/explore/hero-img.png"
           alt=""
-          className="max-w-[400px] mid:max-w-[700px] w-full object-contain"
+          className="max-w-[400px] xl:max-w-[550px] mid:max-w-[700px] w-full object-contain"
         />
       </div>
     </div>
