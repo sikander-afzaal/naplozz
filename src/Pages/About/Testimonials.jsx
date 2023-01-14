b0f063ee0ab6fbfee6853672a277a92bff5f06bf
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-14 22:33:30 +0500

    asd

--- src/Pages/About/Testimonials.jsx
+++ src/Pages/About/Testimonials.jsx
@@ -1,5 +1,4 @@
 import { useRef } from "react";
-import { faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
@@ -19,7 +18,32 @@ const Testimonials = () => {
       sliderRef.current.splide.Components.Controller.go(index);
     }
   };
-
+  const REVIEW_DATA = [
+    {
+      name: "Patrik Panácz (A Grund)",
+      img: "/part3.png",
+      desc: `The online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
+              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities.`,
+    },
+    {
+      name: "Patrik Panácz (A Grund)",
+      img: "/part3.png",
+      desc: `The online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
+              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities.`,
+    },
+    {
+      name: "Patrik Panácz (A Grund)",
+      img: "/part3.png",
+      desc: `The online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
+              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities.`,
+    },
+    {
+      name: "Patrik Panácz (A Grund)",
+      img: "/part3.png",
+      desc: `The online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
+              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities.`,
+    },
+  ];
   return (
     <div className="wrapper mt-[120px] mb-[50px] sm:my-[120px] relative">
       <div className="absolute left-1/2 blur-[120px] -translate-x-1/2 top-[40%] -z-10 bg-[#0262F7] opacity-10 w-full max-w-[1188px] h-[542px]"></div>
@@ -48,36 +72,19 @@ const Testimonials = () => {
           className="review-slider"
           ref={sliderRef}
         >
-          <SplideSlide>
-            <ReviewBox
-              PrevArrow={PrevArrow}
-              NextArrow={NextArrow}
-              name="Patrik Panácz (A Grund)"
-              img="/part3.png"
-              desc="he online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
-              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities."
-            />
-          </SplideSlide>
-          <SplideSlide>
-            <ReviewBox
-              PrevArrow={PrevArrow}
-              NextArrow={NextArrow}
-              name="Patrik Panácz (A Grund)"
-              img="/part3.png"
-              desc="he online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
-              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities."
-            />
-          </SplideSlide>
-          <SplideSlide>
-            <ReviewBox
-              PrevArrow={PrevArrow}
-              NextArrow={NextArrow}
-              name="Patrik Panácz (A Grund)"
-              img="/part3.png"
-              desc="he online logging with Naplozz makes it easier for us to close units and check workflows. The assessment of colleagues and their tasks can be traced.
-              Reminding annual / semi-year / quarterly logs to be less frequent greatly reduced our error opportunities."
-            />
-          </SplideSlide>
+          {REVIEW_DATA.map((elem, idx) => {
+            return (
+              <SplideSlide key={"review" + idx}>
+                <ReviewBox
+                  {...elem}
+                  idx={idx}
+                  totalReviews={REVIEW_DATA.length}
+                  PrevArrow={PrevArrow}
+                  NextArrow={NextArrow}
+                />
+              </SplideSlide>
+            );
+          })}
         </Splide>
       </div>
     </div>
@@ -86,30 +93,39 @@ const Testimonials = () => {
 
 export default Testimonials;
 
-const ReviewBox = ({ img, name, desc, PrevArrow, NextArrow }) => {
+const ReviewBox = ({
+  img,
+  name,
+  desc,
+  PrevArrow,
+  NextArrow,
+  idx,
+  totalReviews,
+}) => {
   return (
     <div className="h-full w-full max-w-[420px] lg:max-w-[820px] relative">
       <div className="grid grid-cols-1 lg:grid-cols-[auto__1fr] px-6 pb-[60px] pt-[30px] lg:py-[40px] gap-x-10 relative rounded-[30px] bg-black w-full h-full">
         {/* arrows---------------- */}
-        <FontAwesomeIcon
-          onClick={PrevArrow}
-          icon={faArrowLeft}
-          className="absolute left-[20px] text-[#B3B3B3] bottom-[20px] text-2xl cursor-pointer"
-        />
-        <FontAwesomeIcon
-          onClick={NextArrow}
-          icon={faArrowRight}
-          className="absolute right-[20px] text-[#B3B3B3] bottom-[20px] text-2xl cursor-pointer"
-        />
+        {idx > 0 && (
+          <FontAwesomeIcon
+            onClick={PrevArrow}
+            icon={faArrowLeft}
+            className="absolute left-[20px] text-[#B3B3B3] bottom-[20px] text-2xl cursor-pointer"
+          />
+        )}
+        {idx < totalReviews - 1 && (
+          <FontAwesomeIcon
+            onClick={NextArrow}
+            icon={faArrowRight}
+            className="absolute right-[20px] text-[#B3B3B3] bottom-[20px] text-2xl cursor-pointer"
+          />
+        )}
         {/* arrows---------------- */}
         <img className="lg:block hidden" src={img} alt="" />
         <div className="flex justify-start items-center sm:text-left text-center sm:items-start flex-col gap-3">
           <h3 className="text-white text-[23px] font-semibold">{name}</h3>
-          <h3 className="text-[#b3b3b3] text-[20px] font-medium">Company</h3>
-          <h3 className="text-[#b3b3b3] text-[20px] font-medium">
-            Job description
-          </h3>
-          <p className="text-white font-medium text-base sm:text-lg">
+
+          <p className="text-white font-medium mt-2 text-base sm:text-lg">
             <span className="text-[42px]">"</span> {desc}{" "}
             <span className="text-[42px]">"</span>
           </p>
